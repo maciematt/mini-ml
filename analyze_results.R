56ba3cd94656d2c279bdff64c8fb95e9e7d84609
pharmhax <atomophile@gmail.com>
2020-01-27 17:15:29 -0500

    swapped out ROC AUC calculations in analyze_results.R to the yardstick implementation

--- analyze_results.R
+++ analyze_results.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(ROCR)
 library(ggplot2)
 library(cowplot)
+library(yardstick)
 
 
 
@@ -19,32 +20,40 @@ local_scores <- function () {
     mods_all <- models[[x]]
     probs_ <- lapply(1:length(mods_all), function (mod_num) {
       mods_all[[mod_num]]$prediction %>% mutate(Resample = mod_num, model = to_analyze[x] %>% str_remove("_model.rds"))
-    }) %>% bind_rows
+    }) %>% bind_rows %>% mutate(ground_truth = ground_truth %>% factor(levels = c("One", "Zero")))
   })
   
   
-  roc_data <- models_probs %>% lapply(function (mod) {
-    rbind(mod, mod %>% mutate(Resample = "All")) %>% group_by(Resample) %>% do(
-      roc = (function (y) {
-        if ((y$ground_truth %>% unique %>% length) != 2)
-          return(data.frame(x = NA, y = NA))
-        z <- prediction(y$Zero, y$ground_truth) %>% performance(measure = "tpr", x.measure = "fpr") ## as a breakpoint we can check that it looks good after "prediction"
-        data.frame(x = z@x.values[[1]], y = z@y.values[[1]])
-      })(.)
-    ) %>% unnest %>% mutate(model = mod[1, "model"]) %>% drop_na
-  }) %>% do.call(rbind, .)
+  #roc_data <- models_probs %>% lapply(function (mod) {
+  #  rbind(mod, mod %>% mutate(Resample = "All")) %>% group_by(Resample) %>% do(
+  #    roc = (function (y) {
+  #      if ((y$ground_truth %>% unique %>% length) != 2)
+  #        return(data.frame(x = NA, y = NA))
+  #      z <- prediction(y$Zero, y$ground_truth) %>% performance(measure = "tpr", x.measure = "fpr") ## as a breakpoint we can check that it looks good after "prediction"
+  #      data.frame(x = z@x.values[[1]], y = z@y.values[[1]])
+  #    })(.)
+  #  ) %>% unnest %>% mutate(model = mod[1, "model"]) %>% drop_na
+  #}) %>% do.call(rbind, .)
 
+
+  roc_data <- models_probs %>% lapply(function (mod) {
+    rbind(mod, mod %>% mutate(Resample = "All")) %>% group_by(Resample) %>% 
+        roc_curve(ground_truth, One) %>% 
+        unnest %>% mutate(model = mod[1, "model"])
+  }) %>% bind_rows
   
-  g <- ggplot() + facet_wrap(~ model) + geom_abline(linetype = "dashed") + geom_line(data = roc_data %>% filter(Resample != "All"), aes(x = x, y = y, group = Resample), alpha = .2, size = .2) + geom_line(data = roc_data %>% filter(Resample == "All"), aes(x = x, y = y), color = "red") + coord_equal() + theme(legend.position = "none") + xlab("False positive rate") + ylab("True positive rate")
+  #g <- ggplot() + facet_wrap(~ model) + geom_abline(linetype = "dashed") + geom_line(data = roc_data %>% filter(Resample != "All"), aes(x = x, y = y, group = Resample), alpha = .2, size = .2) + geom_line(data = roc_data %>% filter(Resample == "All"), aes(x = x, y = y), color = "red") + coord_equal() + theme(legend.position = "none") + xlab("False positive rate") + ylab("True positive rate")
+  g <- ggplot() + facet_wrap(~ model) + geom_abline(linetype = "dashed") + geom_line(data = roc_data %>% filter(Resample != "All"), aes(x = 1 - specificity, y = sensitivity, group = Resample), alpha = .2, size = .2) + geom_line(data = roc_data %>% filter(Resample == "All"), aes(x = 1 - specificity, y = sensitivity), color = "red") + coord_equal() + theme(legend.position = "none") + xlab("False positive rate") + ylab("True positive rate")
   ggsave("./roc_all.pdf", g)
   
-  g <- ggplot(data = roc_data %>% filter(Resample != "All"), aes(x = x, y = y, group = model, fill = model)) + geom_abline(linetype = "dashed") + stat_summary(geom = "ribbon", fun.data = mean_se, alpha = .2) + stat_summary(geom = "line", fun.y = mean, aes(color = model)) + coord_equal() + xlab("False positive rate") + ylab("True positive rate") + xlim(c(0, 1)) + ylim(c(0, 1)) + theme_bw()
+  g <- ggplot(data = roc_data %>% filter(Resample != "All"), aes(x = 1 - specificity, y = sensitivity, group = model, fill = model)) + geom_abline(linetype = "dashed") + stat_summary(geom = "ribbon", fun.data = mean_se, alpha = .2) + stat_summary(geom = "line", fun.y = mean, aes(color = model)) + coord_equal() + xlab("False positive rate") + ylab("True positive rate") + xlim(c(0, 1)) + ylim(c(0, 1)) + theme_bw()
   ggsave("./roc_means.pdf", g)
   
   
   roc_aucs <- models_probs %>% lapply(function (mod) {
     mod %>% group_by(Resample) %>%
-    summarize(auc = ifelse((ground_truth %>% unique %>% length) != 2, NA, performance(prediction(Zero, ground_truth), measure = "auc")@y.values[[1]])) %>% drop_na
+    roc_auc(ground_truth %>% as.factor, One)
+    #summarize(auc = ifelse((ground_truth %>% unique %>% length) != 2, NA, performance(prediction(Zero, ground_truth), measure = "auc")@y.values[[1]])) %>% drop_na
   })
   #names(roc_aucs) <- to_analyze %>% str_remove("_model.RData")
   names(roc_aucs) <- to_analyze %>% str_remove("_model.rds")
